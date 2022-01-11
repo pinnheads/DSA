ca9cad814ebf3cbba8d136d7cdb37ba4a8b71929
Utsav <utsavdeep01@gmail.com>
2022-01-11 20:32:28 +0530

    Binary Search

--- Algo/Search/README.md
+++ Algo/Search/README.md
@@ -16,3 +16,13 @@
 - It is widely used to search an element from the unordered list, i.e., the list in which items are not sorted. The worst-case time complexity of linear search is O(n)
 
 ![Linear Search](linear_search.gif)
+
+## Binary Search
+
+- Binary search is the search technique that works efficiently on sorted lists. Hence, to search an element into some list using the binary search technique, we must ensure that the list is sorted.
+
+- Binary search follows the divide and conquer approach in which the list is divided into two halves, and the item is compared with the middle element of the list.
+
+- If the match is found then, the location of the middle element is returned. Otherwise, we search into either of the halves depending upon the result produced through the match.
+
+![Binary Search](binary-search-sequence-search.gif)
--- Algo/Search/search.py
+++ Algo/Search/search.py
@@ -16,5 +16,39 @@ def linear_search(val, arr):
     return -1
 
 
+def binary_search(val, arr):
+    '''
+    Binary Search Method to search for a element when the element to search and
+    a list are given.
+
+    params:
+        Array/List - Sorted List of elements to search from
+        Value - Element to be searched
+
+    returns:
+        Index of the element found else -1
+    '''
+    if arr != sorted(arr):
+        raise ValueError("Not a sorted list")
+    
+    beg, end = 0, len(arr) - 1
+
+    while beg <= end:
+        mid = (beg+end) // 2
+
+        if arr[mid] == val:
+            return mid
+
+        elif arr[mid] >= val:
+            end = mid - 1
+        
+        else:
+            beg = mid + 1
+        
+    return -1
+
+
 if __name__ == "__main__":
-    print(linear_search(20, [10, 20, 30, 40, 50]))
+    print(f"Linear search {linear_search(22, [21, 22, 23, 25, 27, 29])}")
+    print(f"Binary search: Sorted List {binary_search(22, [21, 22, 23, 24, 25, 26])}")
+    print(f"Binary search: Unsorted List {binary_search(22, [21, 22, 23, 24, 26, 25])}")
